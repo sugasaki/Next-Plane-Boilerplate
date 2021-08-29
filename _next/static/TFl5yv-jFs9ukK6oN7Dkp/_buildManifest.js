5feff3c952d2fa2554e0c5d9610811a6fd0206b9
sugasaki <sugasaki@users.noreply.github.com>
2021-08-29 12:46:27 +0000

    deploy: e5df2e06f403e83da8b7a0eb9dd1199dfe98d934

---
+++ _next/static/TFl5yv-jFs9ukK6oN7Dkp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/c79c70db2c9b54c152eb.css","static/chunks/pages/index-de87438fb05b954b7418.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
